fcab30c014c1ecf7b2dd55b3870a43c4a8afbf16
abhishekgupta <abhishek.gupta@successivesoftwares.com>
2017-01-16 20:33:43 +0530

    changes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -64,8 +64,8 @@ class ApiController extends Controller
                                     $mobileTable = new \App\MobileDetail();
                                     $mobileTable->insertMobileDetail($dataRequest,$documentDetail[0]['id']);
                                     $response['status'] = 'success';
-//                                    $response['data'][] = urlencode($documentDetail[0]['pdf_name']);
-                                    $response['data'][] = urlencode('http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf');
+                                    $response['data'][] = urlencode($documentDetail[0]['pdf_name']);
+//                                    $response['data'][] = urlencode('http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf');
                                     die(json_encode($response));
                                 }else{
                                     $response['status'] = 'error';
